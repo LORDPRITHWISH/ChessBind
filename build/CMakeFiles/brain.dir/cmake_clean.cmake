72988c3b762f5b11d854160fd76774e69a019f50
LORD PRITHWISH <prithwishchakraborty05@gmail.com>
2024-05-13 03:29:34 +0530

    pushed it in

---
+++ build/CMakeFiles/brain.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/brain.dir/computation.cpp.o"
+  "CMakeFiles/brain.dir/computation.cpp.o.d"
+  "brain.cpython-310-x86_64-linux-gnu.so"
+  "brain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/brain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/brain.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for brain.
+# This may be replaced when dependencies are built.
